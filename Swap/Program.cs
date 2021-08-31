bb22d29f715739171865d5d36ac254693813742d
archi-chester <me@qz0.ru>
2021-08-31 21:07:33 +0300

    init

--- Swap/Program.cs
+++ Swap/Program.cs
@@ -1,12 +1,4 @@
-﻿//  Автор - Горбачев Антон aka Archi Chester
-//  Email: me@qz0.ru
-
-//  С# 1
-//  Урок 1
-//  Задача 4
-//  Написать программу обмена значениями двух переменных:
-//  а) с использованием третьей переменной;
-//  б) *без использования третьей переменной.
+﻿.
 
 using System;
 using System.Collections.Generic;
@@ -20,54 +12,6 @@ namespace Swap
     {
         static void Main(string[] args)
         {
-            //  задаем переменные
-            int a = 1;
-            int b = 2;
-
-            //  дергаем функцию с третьей переменной
-            SwapWith3thVar(a, b);
-
-            //  дергаем функцию ,tp третьей переменной
-            SwapWithout3thVar(a, b);
-            
-        }
-
-        //  меняем переменные местами через промежуточную переменную
-        static void SwapWith3thVar(int a, int b)
-        {
-            //  вывод на экран старых значений
-            System.Console.WriteLine($"SwapWith3thVar (old): a={a}, b={b}");
-
-            //  промежуточная переменная
-            int temp;
-
-            //  замена переменных
-            temp = a;
-            a = b;
-            b = temp;
-            
-            //  вывод на экран новых значений
-            System.Console.WriteLine($"SwapWith3thVar (new): a={a}, b={b}");
-            System.Console.WriteLine("Press Enter to Continue");
-            Console.ReadLine();
-
-        }
-
-        //  меняем переменные местами через промежуточную переменную
-        static void SwapWithout3thVar(int a, int b)
-        {
-            
-            //  вывод на экран старых значений
-            System.Console.WriteLine($"SwapWithout3thVar (old): a={a}, b={b}");
-
-            //  замена переменных
-            (a, b) = (b, a);
-            
-            //  вывод на экран новых значений
-            System.Console.WriteLine($"SwapWithout3thVar (new): a={a}, b={b}");
-            System.Console.WriteLine("Press Enter to Continue");
-            Console.ReadLine();
-
         }
 
     }
