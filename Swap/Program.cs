ec199a9d8e086a88ab717123b5a0ff5859dfbbf9
archi-chester <me@qz0.ru>
2021-08-31 19:57:27 +0300

    add swap

--- Swap/Program.cs
+++ Swap/Program.cs
@@ -10,6 +10,55 @@ namespace Swap
     {
         static void Main(string[] args)
         {
+            //  задаем переменные
+            int a = 1;
+            int b = 2;
+
+            //  дергаем функцию с третьей переменной
+            SwapWith3thVar(a, b);
+
+            //  дергаем функцию ,tp третьей переменной
+            SwapWithout3thVar(a, b);
+            
         }
+
+        //  меняем переменные местами через промежуточную переменную
+        static void SwapWith3thVar(int a, int b)
+        {
+            //  вывод на экран старых значений
+            System.Console.WriteLine($"SwapWith3thVar (old): a={a}, b={b}");
+
+            //  промежуточная переменная
+            int temp;
+
+            //  замена переменных
+            temp = a;
+            a = b;
+            b = temp;
+            
+            //  вывод на экран новых значений
+            System.Console.WriteLine($"SwapWith3thVar (new): a={a}, b={b}");
+            System.Console.WriteLine("Press Enter to Continue");
+            Console.ReadLine();
+
+        }
+
+        //  меняем переменные местами через промежуточную переменную
+        static void SwapWithout3thVar(int a, int b)
+        {
+            
+            //  вывод на экран старых значений
+            System.Console.WriteLine($"SwapWithout3thVar (old): a={a}, b={b}");
+
+            //  замена переменных
+            (a, b) = (b, a);
+            
+            //  вывод на экран новых значений
+            System.Console.WriteLine($"SwapWithout3thVar (new): a={a}, b={b}");
+            System.Console.WriteLine("Press Enter to Continue");
+            Console.ReadLine();
+
+        }
+
     }
 }
