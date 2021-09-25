3b7cf0c39de676054051b073fc5b608759b09d18
Archi Chester <me@qz0.ru>
2021-09-25 16:06:23 +0300

    add first carcas

---
+++ Doubler/Program.cs
@@ -0,0 +1,35 @@
+﻿//  *Автор - Горбачев Антон aka Archi Chester
+//  * Email: me @qz0.ru
+
+//  С# 1
+//  Урок 7
+//  Задача 1
+
+//  1. Удвоитель
+//  а) Добавить в программу «Удвоитель» подсчёт количества отданных команд удвоителю.
+//  б) Добавить меню и команду «Играть». При нажатии появляется сообщение, какое число должен получить игрок. Игрок должен получить это число за минимальное количество ходов.
+//  в) *Добавить кнопку «Отменить», которая отменяет последние ходы. Используйте библиотечный обобщенный класс System.Collections.Generic.Stack<int>Stack.
+//  Вся логика игры должна быть реализована в классе с удвоителем.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Doubler
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ TryNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿//  *Автор - Горбачев Антон aka Archi Chester
+//  * Email: me @qz0.ru
+
+//  С# 1
+//  Урок 7
+//  Задача 2
+
+//  2.  Используя Windows Forms, разработать игру «Угадай число». Компьютер загадывает число от 1 до 100, а человек пытается его угадать за минимальное число попыток. Компьютер говорит, больше или меньше загаданное число введенного.
+//  a) Для ввода данных от человека используется элемент TextBox;
+//  б) **Реализовать отдельную форму c TextBox для ввода числа.
+//  Старайтесь разбивать программы на подпрограммы. Переписывайте в начало программы условие и свою фамилию. Все программы сделать в одном решении.
+//  В свойствах проектах в качестве запускаемого проекта укажите “Текущий выбор”.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TryNumber
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
